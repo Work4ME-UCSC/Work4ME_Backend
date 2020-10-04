651cc33d16f1cd24ab4f52319e06127fc2cc9e42
Ram kumar <29110677+Ramkumar96@users.noreply.github.com>
2020-10-04 23:03:25 +0530

    admin-routes

--- src/routers/user.js
+++ src/routers/user.js
@@ -224,4 +224,28 @@ router.get("/:id/avatar", async (req, res) => {
   }
 });
 
+//Admin adding Users
+router.route('/add').post(function (req, res) {
+  console.log(req.body)
+  const user = new User(req.body);
+  user.save()
+    .then(user => {
+      res.status(200).json({ 'user': 'User added successfully' });
+
+    })
+    .catch(err => {
+      res.status(400).send("unable to save to database");
+    });
+});
+
+router.route('/').get(function(req, res) {
+  User.find(function(err, user) {
+      if (err) {
+          console.log(err);
+      } else {
+          res.json(user);
+      }
+  });
+});
+
 module.exports = router;
