29dfe7980a5bad6ae4c4c2c432474cb563bfc895
Christina-Shelani <christinashelani@gmail.com>
2020-10-13 01:24:50 +0530

    added inquiries

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const jobsRoute = require("./routers/jobs");
 const userRouter = require("./routers/user");
 const employeeJobsRouter = require("./routers/employeeJobs");
 const reviewRoute = require("./routers/review");
+const inquiriesRoute = require("./routers/inquiries");
 
 //connecting database
 mongoose.Promise = global.Promise;
@@ -32,6 +33,7 @@ app.use("/jobs", jobsRoute);
 app.use("/users", userRouter);
 app.use("/employee", employeeJobsRouter);
 app.use("/review", reviewRoute);
+app.use("/inquiries", inquiriesRoute);
 
 app.listen(PORT, function () {
   console.log("Server is running on Port: " + PORT);
